201d9680ef2a54aec0c628f2a218cb759a1066a9
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-03-27 11:58:38 +0545

    Create Multidimensionalarray.php

---
+++ Multidimensionalarray.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $arr=array(
+        array (20,25,30)
+        array (10,15,20)
+        array (5,15,25));
+    for ($i=0; $i<3; $i++)
+    {
+        for ($j=0; $j<3; $j++)
+        {
+            echo $arr [i][j]"";
+        }
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
